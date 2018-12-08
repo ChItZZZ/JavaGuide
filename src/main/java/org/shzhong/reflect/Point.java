8c5b3503ff1b5c6040efaad311af82f326f2a5e8
chitzzz <648267970@qq.com>
2018-12-08 19:56:52 +0800

    feat: add test demo and reflect

---
+++ src/main/java/org/shzhong/reflect/Point.java
@@ -0,0 +1,10 @@
+package org.shzhong.reflect;
+ 
+/**
+ * Created by zhongshaohan on 2017/2/21
+ */
+public class Point {
+    static {
+        System.out.println("静态代码块执行：loading point");
+    }
+}
